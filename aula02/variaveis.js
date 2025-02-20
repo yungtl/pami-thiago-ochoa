7d5b2b53aec88468ae1c5005861ded2371b0fe35
thiago ochoa <ochoathiago454@gmail.com>
2025-02-20 08:29:11 -0300

    aula sobre tipo de variaveis

---
+++ aula02/variaveis.js
@@ -0,0 +1,13 @@
+var numero =2;
+let texto = "hello";
+const oQueEisso = "?";
+
+console.log('tipo var:', numero);
+// contactear com virgula me garante um espaço
+numero = 13;
+
+console.log(texto + " o numero agora é " + numero)
+
+texto = 69
+
+console.log(texto+numero)
